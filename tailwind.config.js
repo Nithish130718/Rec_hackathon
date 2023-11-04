d70c28d4ac96214ef775f3fed6be4ea2ad22b497
Nithish130718 <145916416+Nithish130718@users.noreply.github.com>
2023-11-04 23:31:52 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// Use the ESM export syntax to export your configuration object
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
